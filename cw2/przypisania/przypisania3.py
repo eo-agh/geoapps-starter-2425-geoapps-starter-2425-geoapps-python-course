1ef5739b68cf527f9b164c8079eae2a7edbab2d9
Martyna <msala983@gmail.com>
2025-03-17 16:49:29 +0000

    przypisania

---
+++ cw2/przypisania/przypisania3.py
@@ -0,0 +1,15 @@
+K = [1, 2]
+L = K
+# konkatenacja
+K = K + [3, 4]
+M = [1, 2]
+N = M
+print(f"K: {K}; L: {L}; M: {M}; N: {N}")
+# przypisanie rozszerzone
+M += [3, 4]
+
+print(f"K: {K}; L: {L}; M: {M}; N: {N}")
+
+#kontrachencja laczy ciagi znakow, sekwencje lub struktury danych w jeden dluzszy ciag/strukture, 
+# a przypisania rozszerzone modyfikują wartość zmiennej i przypisują wynik
+
