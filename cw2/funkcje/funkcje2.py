984595a4314c0451bc24232bff90ac3ec5fd148d
Martyna <msala983@gmail.com>
2025-03-23 20:21:29 +0000

    funkcje2

---
+++ cw2/funkcje/funkcje2.py
@@ -0,0 +1,15 @@
+def zamowienie_produktu(nazwa_produktu, *, cena, ilosc = 1):
+    wartosc_zamowienia = cena*ilosc
+    podsumowanie = f"Podsumowanie:\nProdukt: {nazwa_produktu}.\nLaczna cena: {wartosc_zamowienia}.\nIlosc zamowionego produktu: {ilosc}.\n"
+    return podsumowanie, wartosc_zamowienia
+    
+zamowienia = []
+zamowienia.append(zamowienie_produktu("lodowka", cena=3500, ilosc=2))
+zamowienia.append(zamowienie_produktu("pralka", cena=2000, ilosc=1))
+zamowienia.append(zamowienie_produktu("mikser", cena=300, ilosc=3))
+
+for podsumowanie, _ in zamowienia:
+    print(podsumowanie)
+
+suma_zamowien = sum(zamowienie for _, zamowienie in zamowienia)
+print(f"Łączna wartość wszystkich zamówień: {suma_zamowien:.2f} zł")
