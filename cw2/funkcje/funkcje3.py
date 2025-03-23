9a03e3b7605bc64ffe3d6ac6ff0e5f737520f30d
Martyna <msala983@gmail.com>
2025-03-23 20:49:34 +0000

    funkcje3 - wstep

---
+++ cw2/funkcje/funkcje3.py
@@ -0,0 +1,12 @@
+def stworz_raport(*args, **kwargs):
+    for id in args:
+        info = dict()
+        for key, value in kwargs.items():
+            key_r = key.replace("_", " ")
+            key_rs = key_r.split()
+            if int(key_rs[1]) == id:
+                info.update({key_rs[0]:value})
+        print(f"Raport produktu o nr ID: {id}\n{info}")
+        del info
+            
+stworz_raport(101, 102, nazwa_101="Kubek termiczny", cena_101="45.99 zł", nazwa_102="Długopis", cena_102="4.99 zł")
