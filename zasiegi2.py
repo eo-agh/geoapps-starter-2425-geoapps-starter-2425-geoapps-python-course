5fc5f860123ace04b5428b1099fcfa39a6ba8a8b
Martyna <msala983@gmail.com>
2025-04-07 08:05:38 +0000

    zadania -- zasiegi

---
+++ zasiegi2.py
@@ -0,0 +1,49 @@
+def licznik_nonlocal():
+    licznik_stanu_nonlocal = 0
+    
+    def zwieksz():
+        nonlocal licznik_stanu_nonlocal
+        licznik_stanu_nonlocal += 1
+        return licznik_stanu_nonlocal
+    return zwieksz
+ 
+li_pr1 = licznik_nonlocal()
+print("Licznik nonlocal:")
+print(li_pr1())
+print(li_pr1())
+
+
+licznik_stanu_global = 0
+
+def licznik_global():
+    global licznik_stanu_global
+    licznik_stanu_global += 1
+    return licznik_stanu_global
+
+print("Licznik globalny:")
+print(licznik_global())
+print(licznik_global())
+
+
+class Licznik_class:
+    def __init__(self):
+        self.licznik_stanu_class = 0
+
+    def __call__(self):
+        self.licznik_stanu_class += 1
+        return self.licznik_stanu_class
+
+li_pr2 = Licznik_class()
+print("Licznik klasy:")
+print(li_pr2())  
+print(li_pr2()) 
+
+
+def licznik_funkcyjny():
+    licznik_funkcyjny.licznik_f += 1
+    return licznik_funkcyjny.licznik_f
+
+licznik_funkcyjny.licznik_f = 0
+print("Licznik funkcyjny:")
+print(licznik_funkcyjny())
+print(licznik_funkcyjny())
